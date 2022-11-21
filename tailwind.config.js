2b6d92fbd19583bf4b478a6823b3f08a64897c31
Jahir514 <jahirhasan.amana@gmail.com>
2022-11-21 17:59:26 +0600

    working

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,js}'],
+  content: [
+    "./pages/*.{html,js}",
+    "./*.{html,js}"
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
