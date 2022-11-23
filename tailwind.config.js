77a490d049dac4a6d18a716608d9a3bcb5714588
Jahir514 <jahirhasan.amana@gmail.com>
2022-11-23 08:22:02 +0600

    tailwind setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,js}'],
+  content: ['./src/**/*.{html,js}', './*.{html,js}'],
   theme: {
-    extend: {},
+    fontFamily: {
+      poppins: ['poppins, sans-serif'],
+    },
+    container: {
+      center: true,
+      padding: '1rem',
+    },
+    extend: {
+      colors: {
+        'light-green': '#7ed56f',
+        green: '#55c57a',
+        'dark-green': '#28b485',
+        grey: '#777777',
+        'dark-grey': '#333333',
+        'light-grey': '#f7f7f7',
+        'light-orange': '#ffb900',
+        'dark-orange': '#ff7730',
+        'light-blue': '#2998ff',
+        'dark-blue': '#5643fa',
+        secondary: '#212529',
+        'btn-color': '#00bcb3',
+      },
+    },
   },
   plugins: [],
 };
